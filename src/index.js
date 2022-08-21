4b016d4ecd7bd9ec624e3f101269b844a0250da1
Marco14395 <theevilmcpe@gmail.com>
2022-08-22 02:05:55 +0800

    Finished Exercise 6-1 6-2

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>ok</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={zero}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+      <div>all {store.getState().good + store.getState().ok + store.getState().bad}</div>
+      <div>average {Number(( store.getState().good - store.getState().bad ) / (store.getState().good + store.getState().ok + store.getState().bad))}</div>
+      <div>positive {Number(( store.getState().good ) / (store.getState().good + store.getState().ok + store.getState().bad)) * 100 +"%"}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
